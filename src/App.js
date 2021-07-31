d3905f426db519a36c197ac8a4d54651139720bc
Jose Guevara <josexx1234@gmail.com>
2021-07-31 17:38:48 -0400

    Add homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Homepage from "./components/Homepage.component";
 
 function App() {
-  return <div className="bg-gray-200">Hello world</div>;
+  return (
+    <div className="bg-gray-200">
+      <Homepage />
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,16 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      minWidth: { "3/10": "30%" },
+      margin: { "6px": "6px" },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      borderWidth: ["first", "last"],
+      margin: ["first", "last"],
+    },
   },
   plugins: [],
 };
