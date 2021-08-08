8dbb3419aa43cfb33f9586d8e55f8164e4c2bbbe
Jose Guevara <josexx1234@gmail.com>
2021-08-08 09:59:07 -0400

    updating our app to redirect to home if user is signeid in

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
-import { Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 import Header from "./components/header/Header.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import Authentication from "./pages/authentication/Authentication.component";
@@ -9,7 +9,7 @@ import ShopPage from "./pages/shop/ShopPage.component";
 import { setCurrentUser } from "./redux/user/user.actions";
 import "./tailwind.css";
 
-function App({ setCurrentUser }) {
+function App({ currentUser, setCurrentUser }) {
   useEffect(() => {
     const unSubscribe = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
@@ -30,19 +30,31 @@ function App({ setCurrentUser }) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  useEffect(() => {
+    console.log(currentUser);
+  }, [currentUser]);
+
   return (
     <div className="px-16 py-5 font-open-condense">
       <Header />
       <Switch>
         <Route exact path={"/"} component={Homepage} />
         <Route path={"/shop"} component={ShopPage} />
-        <Route path={"/signin"} component={Authentication} />
+        <Route
+          exact
+          path={"/signin"}
+          render={() =>
+            currentUser ? <Redirect to="/" /> : <Authentication />
+          }
+        />
       </Switch>
     </div>
   );
 }
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
-
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
